302d78801d673135b0842e497dfe460b807b23c4
Josh Mo <joshmomel@gmail.com>
2023-02-04 16:37:44 +1100

    feat: print a page table

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,38 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void vmprinthelper(pagetable_t, int);
+void vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", PTE2PA(pagetable[0]));
+  vmprinthelper(pagetable, 0);
+}
+
+void vmprinthelper(pagetable_t pagetable, int space)
+{
+  for (int i = 0; i < 512; i++)
+  {
+    pte_t pte = pagetable[i];
+    if (pte == 0)
+    {
+      continue;
+    }
+
+    for (int j = space; j >= 0; j--)
+    {
+      printf(" ..");
+    }
+    printf("%d: ", i);
+    printf("pte %p pa %p\n", pte, PTE2PA(pte));
+
+    if ((pte & PTE_V) && (pte & (PTE_R | PTE_W | PTE_X)) == 0)
+    {
+      // this PTE points to a lower-level page table.
+      uint64 child = PTE2PA(pte);
+
+      vmprinthelper((pagetable_t)child, ++space);
+      space--;
+    }
+  }
+}
