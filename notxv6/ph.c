25e0acbdfc368346ad33c039486702d4fed878e6
Josh Mo <joshmomel@gmail.com>
2023-03-19 14:13:15 +1100

    Using threads

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -16,6 +16,7 @@ struct entry {
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
+pthread_mutex_t mutexes[NBUCKET]; 
 
 
 double
@@ -43,6 +44,8 @@ void put(int key, int value)
 
   // is the key already present?
   struct entry *e = 0;
+  // 获取桶对应的互斥锁
+  pthread_mutex_lock(&mutexes[i]);
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key)
       break;
@@ -54,7 +57,8 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
-
+  // 释放桶对应的互斥锁
+  pthread_mutex_unlock(&mutexes[i]);
 }
 
 static struct entry*
@@ -64,10 +68,14 @@ get(int key)
 
 
   struct entry *e = 0;
+   // 获取桶对应的互斥锁
+  pthread_mutex_lock(&mutexes[i]);
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key) break;
   }
 
+  // 释放桶对应的互斥锁
+  pthread_mutex_unlock(&mutexes[i]);
   return e;
 }
 
@@ -104,6 +112,10 @@ main(int argc, char *argv[])
   pthread_t *tha;
   void *value;
   double t1, t0;
+  // 初始化互斥锁数组
+  for (int i = 0; i < NBUCKET; i++) {
+    pthread_mutex_init(&mutexes[i], NULL);
+  }
 
 
   if (argc < 2) {
